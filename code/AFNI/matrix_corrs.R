c5298d121e4b4fd30b1e8ddeca82e7a7f348070c
Lindsay Oliver <lindsaydoliv@gmail.com>
2020-07-14 18:51:10 -0400

    Updated script to read in new nistats GLM outputs (regressors across runs vs by run).

--- code/AFNI/matrix_corrs.R
+++ code/AFNI/matrix_corrs.R
@@ -12,39 +12,42 @@ spm_mat_list <- list.files(path= ".", recursive=T, pattern="^sub.*spm_mat\\.csv$
 afni_mat_list <- list.files(path= "./", recursive=T, pattern="^sub.*mat.xmat\\.1D$")
 
 # find nistats design matrices for each participant
-nistats_mat_list <- list.files(path= "/projects/ttan/fMRI_tools/compare_task_tools/nistats",recursive=T,full.names=T, 
-                               pattern="^sub.*emp_combined\\.tsv$")
+nistats_mat_list <- list.files(path= "/projects/ttan/fMRI_tools/analysis/first_lvl",recursive=T,full.names=T, 
+                               pattern="^sub.*emp_combined_dm\\.tsv$")
 
 # read in mats
 spm_mat <- lapply(spm_mat_list, read.csv, header=F, col.names=c("ea_block","ea_pmod","circles","ea_press","circ_press",
-                    rep("confounds",55)))
+                    rep("confounds",19)))
 
 afni_mat <- lapply(afni_mat_list, read.table, header=F, skip=32, sep=" ", col.names=c("skip",rep("polort",18),"ea_block",
                     "ea_pmod","circles","ea_press","circ_press","FD","x","y","z","roll","pitch","yaw","xder","yder","zder",
                     "rollder","pitchder","yawder","mean_wm","mean_csf"))
 
 nistats_mat <- lapply(nistats_mat_list, read.table, header=F, skip=1, col.names=c("skip","ea_block","ea_press","circles",
-                    "circ_press","ea_pmod",rep("confounds",73)))
+                    "circ_press","ea_pmod","mean_csf","mean_wm","FD","trans_x","trans_x_der","trans_y",
+                    "trans_y_der","trans_z","trans_z_der","rot_x","rot_x_der","rot_y","rot_y_der","rot_z","rot_z_der",
+                    "drift_1","drift_2","drift_3","drift_4","drift_5","constant"))
+
 
 # add subject ids
 names(spm_mat) <- substring(spm_mat_list,1,11)
 names(afni_mat) <- substring(afni_mat_list,1,11)
-names(nistats_mat) <- substring(nistats_mat_list,54,64)
+names(nistats_mat) <- substring(nistats_mat_list,58,68)
 
-# collapse values across runs (SPM and nistats) and remove unwanted columns
-for (i in names(spm_mat)) {
-  spm_mat[[i]][274:546,1:19] <- spm_mat[[i]][274:546,20:38]
-  spm_mat[[i]][547:819,1:19] <- spm_mat[[i]][547:819,39:57]
-}
+# collapse values across runs (SPM and nistats - not necessary with changes) and remove unwanted columns
+#for (i in names(spm_mat)) {
+#  spm_mat[[i]][274:546,1:19] <- spm_mat[[i]][274:546,20:38]
+#  spm_mat[[i]][547:819,1:19] <- spm_mat[[i]][547:819,39:57]
+#}
 
 for (i in names(nistats_mat)) {
-  nistats_mat[[i]] <- nistats_mat[[i]][,2:79]
-  nistats_mat[[i]][274:546,1:26] <- nistats_mat[[i]][274:546,27:52]
-  nistats_mat[[i]][547:819,1:26] <- nistats_mat[[i]][547:819,53:78]
+  nistats_mat[[i]] <- nistats_mat[[i]][,-1]
+#  nistats_mat[[i]][274:546,1:26] <- nistats_mat[[i]][274:546,27:52]
+#  nistats_mat[[i]][547:819,1:26] <- nistats_mat[[i]][547:819,53:78]
 }
 
 for (i in names(afni_mat)) {
-  afni_mat[[i]] <- afni_mat[[i]][,2:39]
+  afni_mat[[i]] <- afni_mat[[i]][,-1]
 }
 
 # correlate columns across programs for each participant
