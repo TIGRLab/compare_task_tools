4a2c4a38a1dd6681bb8a9c1fe025ef070439f5fa
Lindsay Oliver <lindsaydoliv@gmail.com>
2020-07-16 16:15:10 -0400

    Added code to visualize/compare design matrices across programs.

--- code/AFNI/matrix_corrs.R
+++ code/AFNI/matrix_corrs.R
@@ -1,6 +1,7 @@
 
 library(lineup)
 library(ggplot2)
+library(reshape2)
 
 # set working dir
 setwd("/projects/loliver/SPINS_GLM_Test")
@@ -14,6 +15,7 @@ afni_mat_list <- list.files(path= "./", recursive=T, pattern="^sub.*mat.xmat\\.1
 # find nistats design matrices for each participant
 nistats_mat_list <- list.files(path= "/projects/ttan/fMRI_tools/analysis/first_lvl",recursive=T,full.names=T, 
                                pattern="^sub.*emp_combined_dm\\.tsv$")
+nistats_mat_list <- nistats_mat_list[1:10]  # cut ones in tmp folder
 
 # read in mats
 spm_mat <- lapply(spm_mat_list, read.csv, header=F, col.names=c("ea_block","ea_pmod","circles","ea_press","circ_press",
@@ -35,19 +37,22 @@ names(afni_mat) <- substring(afni_mat_list,1,11)
 names(nistats_mat) <- substring(nistats_mat_list,58,68)
 
 # collapse values across runs (SPM and nistats - not necessary with changes) and remove unwanted columns
-#for (i in names(spm_mat)) {
-#  spm_mat[[i]][274:546,1:19] <- spm_mat[[i]][274:546,20:38]
-#  spm_mat[[i]][547:819,1:19] <- spm_mat[[i]][547:819,39:57]
-#}
+for (i in names(spm_mat)) {
+  spm_mat[[i]]$TR <- 1:819
+  #  spm_mat[[i]][274:546,1:19] <- spm_mat[[i]][274:546,20:38]
+  #  spm_mat[[i]][547:819,1:19] <- spm_mat[[i]][547:819,39:57]
+}
 
 for (i in names(nistats_mat)) {
   nistats_mat[[i]] <- nistats_mat[[i]][,-1]
-#  nistats_mat[[i]][274:546,1:26] <- nistats_mat[[i]][274:546,27:52]
-#  nistats_mat[[i]][547:819,1:26] <- nistats_mat[[i]][547:819,53:78]
+  nistats_mat[[i]]$TR <- 1:819
+  #  nistats_mat[[i]][274:546,1:26] <- nistats_mat[[i]][274:546,27:52]
+  #  nistats_mat[[i]][547:819,1:26] <- nistats_mat[[i]][547:819,53:78]
 }
 
 for (i in names(afni_mat)) {
   afni_mat[[i]] <- afni_mat[[i]][,-1]
+  afni_mat[[i]]$TR <- 1:819
 }
 
 # correlate columns across programs for each participant
@@ -66,7 +71,44 @@ for (i in names(spm_mat)) {
 
 all_corrs <- all_corrs[,c(1,6,11,2,7,12,3,8,13,4,9,14,5,10,15)]
 
-#write.csv(all_corrs,file="/projects/loliver/SPINS_GLM_Test/design_mat_corrs.csv",row.names = T)
+#write.csv(all_corrs,file="/projects/loliver/SPINS_GLM_Test/design_mat_corrs_fixed.csv",row.names = T)
+
+
+# melt df to visualize across programs
+spm_mat_long <- lapply(spm_mat, melt, id.vars = "TR")
+afni_mat_long <- lapply(afni_mat, melt, id.vars = "TR")
+nistats_mat_long <- lapply(nistats_mat, melt, id.vars = "TR")
+
+all_mats <- vector(mode = "list", length = 9)
+names(all_mats) <- names(spm_mat)
+
+for (i in names(spm_mat)) {
+  spm_mat_long[[i]]$program <- c(rep("SPM",19656))
+  afni_mat_long[[i]]$program <- c(rep("afni",31122))
+  nistats_mat_long[[i]]$program <- c(rep("ni",21294))
+  all_mats[[i]] <- rbind(spm_mat_long[[i]],afni_mat_long[[i]],nistats_mat_long[[i]])
+}
+
+
+# visualize 
+
+for (i in names(spm_mat)) {
+   ggplot(data.frame(all_mats[[i]][all_mats[[i]]$variable=="ea_pmod",]), aes(x = TR, y = value, col=program)) + 
+   geom_line()
+}
+  
+ggplot(data.frame(all_mats[["sub-CMH0057"]][all_mats[["sub-CMH0057"]]$variable=="ea_pmod",]), aes(x = TR, y = value, col=program)) + 
+  geom_line() #+ facet_wrap("variable")
+
+ggplot(data.frame(all_mats[["sub-CMH0057"]][all_mats[["sub-CMH0057"]]$variable=="ea_block",]), aes(x = TR, y = value, col=program)) + 
+  geom_line() #+ facet_wrap("variable")
+
+ggplot(data.frame(all_mats[["sub-CMH0065"]][all_mats[["sub-CMH0065"]]$variable=="ea_pmod",]), aes(x = TR, y = value, col=program)) + 
+  geom_line() #+ facet_wrap("variable")
+
+ggplot(data.frame(all_mats[["sub-CMH0093"]][all_mats[["sub-CMH0093"]]$variable=="ea_pmod",]), aes(x = TR, y = value, col=program)) + 
+  geom_line() #+ facet_wrap("variable")
+
 
 
 # visualize 
@@ -97,15 +139,4 @@ ggplot(data.frame(afni_mat[["sub-CMH0093"]]), aes(x = 1:819, y = ea_pmod)) +
 ggplot(data.frame(nistats_mat[["sub-CMH0093"]]), aes(x = 1:819, y = ea_pmod)) + 
   geom_line()
 
-new0057 <- read.csv(file="/projects/loliver/SPINS_GLM_Test/sub-CMH0057/sub-CMH0057_spm_mat.csv",header=F, col.names=c("ea_block","ea_pmod","circles","ea_press","circ_press",
-                                                                                                           rep("confounds",58)))
-
-ggplot(data.frame(new0057[1:273,]), aes(x = 1:273, y = ea_press)) + 
-  geom_line()
-
-ggplot(data.frame(afni_mat[["sub-CMH0057"]][1:273,]), aes(x = 1:273, y = ea_press)) + 
-  geom_line()
-
-ggplot(data.frame(nistats_mat[["sub-CMH0057"]][1:273,]), aes(x = 1:273, y = ea_press)) + 
-  geom_line()
 
